508611f84620bf5f21bd0e21460122c656cc1fc2
hcopp <hcopp@DESKTOP-SF67LSQ>
2020-11-14 12:28:54 +0100

    Add project files.

---
+++ Demo_struct_Boek/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Demo_struct_Boek
+{
+
+    class Program
+    {
+        struct Boek
+        {
+            public double Prijs; //veld of field
+            public string Auteur;//veld of field
+            public string Titel;//veld of field
+            //constructor maken: wordt aangeroepen wanneer je een new Boek()
+            //dient voornamelijk om de velden te initialiseren
+            public Boek(double prijs, string titel, string auteur)
+            {
+                Prijs = prijs;
+                Auteur = auteur;
+                Titel = titel;
+            }
+            //methode PrintBoek
+            public void PrintBoek()
+            {
+                Console.WriteLine($"Boek '{Titel}' van {Auteur} Prijs: {Prijs}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Boek boek1 = new Boek(15.5, "Harry Potter - De steen der wijzen", "J.K. Rowling");
+            boek1.PrintBoek();
+            //Boek boek1 = new Boek();
+            //boek1.Prijs = 15.5;
+            //boek1.Titel = "Harry Potter - De steen der wijzen";
+            //boek1.Auteur = "J.K. Rowling";
+            Boek boek2 = new Boek(35.15, "Pro C# 8", "A. Wesley");
+            boek2.PrintBoek();
+            //Boek boek2 = new Boek();
+            //boek2.Prijs = 35.15;
+            //boek2.Titel = "Pro C# 8";
+            //boek2.Auteur = "A. Wesley";
+
+            //Console.WriteLine($"Titel van boek1: {boek1.Titel}");
+            //Console.WriteLine($"Titel van boek2: {boek2.Titel}");
+            //Console.WriteLine($"Prijs van de 2 boeken: {boek1.Prijs + boek2.Prijs}");
+        }
+    }
+}
---
+++ Demo_struct_Kaart/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Demo_struct_Kaart
+{
+    class Program
+    {
+        enum Kleur { Rood, Zwart}
+        enum Teken { Schoppen, Ruiten, Klaveren, Harten}
+        enum Rang { Aas, Twee ,Drie,Vier, Vijf,Zes,Zeven,Acht,Negen,Tien,Boer, Dame,Heer,Joker}
+        struct Kaart
+        {
+            Kleur Kaart_Kleur;
+            Teken Kaart_Teken;
+            Rang Kaart_Rang;
+            //constructor voor Kaart
+            public Kaart(Kleur kleur, Teken teken, Rang rang)
+            {
+                Kaart_Kleur = kleur;
+                Kaart_Rang = rang;
+                Kaart_Teken = teken;
+            }
+            //methode om gegevens kaart af te printen 
+            public void PrintKaart()
+            {
+                Console.WriteLine($"Kaart is: {Kaart_Kleur} {Kaart_Teken} {Kaart_Rang}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Kaart kaart1 = new Kaart(Kleur.Rood, Teken.Harten, Rang.Dame);
+            kaart1.PrintKaart();
+
+            Kaart kaart2 = new Kaart(Kleur.Zwart, Teken.Schoppen, Rang.Boer);
+            kaart2.PrintKaart();
+        }
+    }
+}
